798892a04f1f4d8c728b6f1520f203cf2f332cbf
Chase T <chaysonthelen@gmail.com>
2026-04-10 10:33:52 -0700

    commit3

--- mods/ChaseContainer/LootContainer.gd
+++ mods/ChaseContainer/LootContainer.gd
@@ -1,33 +1,17 @@
 extends "res://Scripts/LootContainer.gd"
 
 var searched = false
-var isCabin = false
-var current_scene: Node
-
-func _ready():
-    super()
-    print("---------------TEST-----------------")
-    current_scene = get_tree().current_scene
-    isCabin = current_scene.name == "Cabin"
 
 func Interact():
-
+    super()
     if !locked:
-        var UIManager = get_tree().current_scene.get_node("/root/Map/Core/UI")
-        UIManager.OpenContainer(self)
-        ContainerAudio()
         searched = true
 
 func UpdateTooltip():
     if locked:
         gameData.tooltip = containerName + " [Locked]"
-    elif isCabin:
-        if loot.is_empty() and storage.is_empty():
-            gameData.tooltip = containerName + " [Empty]"
-        else:
-            gameData.tooltip = containerName + " [Open]"
     else:
         if searched and loot.is_empty() and storage.is_empty():
             gameData.tooltip = containerName + " [Empty]"
         else:
-            gameData.tooltip = containerName + " [Open]"
+            gameData.tooltip = containerName + " [Open Gay Ass]"
